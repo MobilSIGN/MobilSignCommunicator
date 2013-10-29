2276f0028a18777d74925745eb64819b5e445fcd
Peter Adamek <adamek.peter@gmail.com>
2013-10-29 17:07:38 +0100

    TESTOVANIE KODOVANIA A DEKODOVANIA RSA cez BASE 64. Zatial nefunguje.

--- src/communicator/Crypto.java
+++ src/communicator/Crypto.java
@@ -1,26 +1,23 @@
 package communicator;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
-import sun.misc.BASE64Decoder;
+import org.apache.commons.codec.binary.Base64;
 
 public class Crypto {
 
     public static final String ALGORITHM = "RSA";
     private Cipher mCipher;
-//    private PublicKey mPublicKey;
-//    private PrivateKey mPrivateKey;
     private Key mKey;
 
     // Parameter key je bud PublicKey alebo PrivateKey.
@@ -33,23 +30,6 @@ public class Crypto {
         }
     }
 
-//    public Crypto(PublicKey publicKey) {
-//        mPublicKey = publicKey;
-//        try {
-//            mCipher = Cipher.getInstance(ALGORHYTM);
-//        } catch (NoSuchAlgorithmException | NoSuchPaddingException ex) {
-//            Logger.getLogger(Crypto.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//    }
-//
-//    public Crypto(PrivateKey privateKey) {
-//        mPrivateKey = privateKey;
-//        try {
-//            mCipher = Cipher.getInstance(ALGORHYTM);
-//        } catch (NoSuchAlgorithmException | NoSuchPaddingException ex) {
-//            Logger.getLogger(Crypto.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//    }
     public byte[] encrypt(byte[] text) {
         byte[] cipherText = null;
         try {
@@ -64,23 +44,28 @@ public class Crypto {
 
     public String encrypt(String text) {
         byte[] b = null;
+        List<byte[]> list = null;
+        List<byte[]> encryptedList = null;
         try {
-            b = this.encrypt(text.getBytes("UTF-8"));
+            list = divideByteArray(text.getBytes("UTF-8"), 245);
         } catch (UnsupportedEncodingException ex) {
             Logger.getLogger(Crypto.class.getName()).log(Level.SEVERE, null, ex);
         }
-        try {
-            return new String(b, "UTF-8");
-        } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(Crypto.class.getName()).log(Level.SEVERE, null, ex);
+        encryptedList = new ArrayList<>();
+        for (byte[] iter : list) {
+            encryptedList.add(this.encrypt(iter));
         }
-        return null;
+        //        try {
+        //            return new String(b, "UTF-8");
+        //        } catch (UnsupportedEncodingException ex) {
+        //            Logger.getLogger(Crypto.class.getName()).log(Level.SEVERE, null, ex);
+        //        }
+        return encodeByteToBase64String(splitByteArray(encryptedList, 245));
     }
-    
+
     // todo
     // delenie na 245 bajtov
     // urobit decode stringu na bajty a naopak
-
     public byte[] decrypt(byte[] text) {
         byte[] dectyptedText = null;
         try {
@@ -97,39 +82,122 @@ public class Crypto {
         byte[] b = null;
         try {
             b = text.getBytes("UTF-8");
-            if (b == null) {
-                System.out.println("1 null");
-            }
-            b = this.decrypt(b);
-            if (b == null) {
-                System.out.println("2 null");
-            }
-        } catch (Exception e) {
-            System.err.println("Decrypt padol 2.");
-        }
-        try {
-            return new String(b, "UTF-8");
-//            return decodeToBASE64(b);
         } catch (UnsupportedEncodingException ex) {
             Logger.getLogger(Crypto.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return null;
+        List<byte[]> list = divideByteArray(b, 245);
+        List<byte[]> list2 = new ArrayList<>();
+        for (byte[] iter : list) {
+            list2.add(this.decrypt(iter));
+        }
+        byte[] vysl = splitByteArray(list2, 245);
+        return encodeByteToBase64String(vysl);
     }
 
     public static void main(String[] args) {
-        try {
-            String s = "hahaha";
-            System.out.println(s);
-            byte[] b = s.getBytes("UTF-8");
-            System.out.println(b);
-            try {
-                String str = new String(b, "UTF-8");
-                System.out.println(str);
-            } catch (UnsupportedEncodingException ex) {
-                Logger.getLogger(Crypto.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(Crypto.class.getName()).log(Level.SEVERE, null, ex);
+        byte b[] = new byte[246];
+        for (int i = 0; i < 246; i++) {
+            b[i] = 1;
         }
+        String str = "952t9852udifg dfsi fius dfius ffsfsfsdo sofj sodfjsj fsiojdf sof osindf oisndfiosn fosnf sndfoisdnfo insfodi nsofn oisnf iosnfiosnf iosnfoidsnfoisnfoisnfoisnfoisn foisnfiosn foisn fosi nfos noinsfoi snoifsnonsf onsfo nsoidfn sionfdiosnsfoindosi n";
+        System.out.println(str);
+//        System.out.println(str);
+////        for (int i = 0; i < 244; i++) {
+////            str += new char(i);
+////        }
+//        List<byte[]> l = new ArrayList<>();
+//        byte[] bytes = str.getBytes();
+//        for (byte iter : bytes) {
+//            System.out.print(iter + ", ");
+//        }
+//        l = divideByteArray(bytes, 245);
+//        System.out.println("L ma prvkov: " + l.size());
+//        byte[] bytes2 = splitByteArray(l, 245);
+//        for (byte iter : bytes2) {
+//            System.out.print(iter + ", ");
+//        }
+//        System.out.println("");
+//        System.out.println(new String(bytes2));
+//       try {
+//            String s = "hahaha";
+//            System.out.println(s);
+//            byte[] b = s.getBytes("UTF-8");
+//            System.out.println(b);
+//            try {
+//                String str = new String(b, "UTF-8");
+//                System.out.println(str);
+//            } catch (UnsupportedEncodingException ex) {
+//                Logger.getLogger(Crypto.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//        } catch (UnsupportedEncodingException ex) {
+//            Logger.getLogger(Crypto.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+    }
+
+    private static byte[] splitByteArray(List<byte[]> l, int pieceSize) {
+        int size = 0;
+        for (byte[] iter : l) {
+            size += iter.length;
+        }
+        byte[] temp = new byte[size];
+        for (int i = 0; i < l.size(); i++) {
+            for (int j = 0; j < l.get(i).length; j++) {
+                temp[i * pieceSize + j] = l.get(i)[j];
+            }
+//            System.arraycopy(l.get(i), 0, temp, i * pieceSize, l.get(i).length);
+        }
+        return temp;
+    }
+
+    private static List<byte[]> divideByteArray(byte[] b, int pieceSize) {
+        List<byte[]> list = new ArrayList<>();
+        byte[] temp = null;
+        boolean zacalSom = false;
+        System.out.println("mATH: " + Math.ceil(b.length / (pieceSize * 1.0)));
+        System.out.println("Dlzka pola je " + b.length);
+        for (int i = 0; i < Math.ceil(b.length / (pieceSize * 1.0)); i++) {
+            System.out.println("###");
+            if (i * pieceSize + pieceSize < b.length) {
+                temp = new byte[pieceSize];
+                System.arraycopy(b, i * pieceSize, temp, 0, pieceSize);
+                System.out.println("IDEM 1 OD " + i * pieceSize + " kolko: " + pieceSize);
+            } else {
+                if (b.length <= pieceSize) {
+                    temp = new byte[b.length];
+                    System.arraycopy(b, i * pieceSize, temp, 0, b.length);
+                    System.out.println("IDEM 2 OD " + i * pieceSize + " kolko " + b.length);
+                } else {
+                    temp = new byte[b.length - i * pieceSize];
+                    System.arraycopy(b, i * pieceSize, temp, 0, b.length - i * pieceSize);
+                    System.out.println("IDEM 3 OD " + i * pieceSize + " kolko " + (b.length - i * pieceSize));
+                }
+            }
+            list.add(temp);
+        }
+
+//        for (int i = 0; i < b.length; i++) {
+//            if (zacalSom && (i % pieceSize == 0 || i == b.length- 1)) {
+//                list.add(temp);
+//                
+//            } else {
+//                temp[i % pieceSize] = b[i];
+//            }
+//           zacalSom = true; 
+//        }
+        return list;
+    }
+
+    /**
+     * dekoduje base64 retazec na subor a ten vrati v podobe pola bajtov
+     *
+     * @param base64String retazet reprezentujuci subor
+     * @return rozkodovany subor vo forme pola bajtov
+     */
+    public byte[] decodeBase64StringToByte(String string) {
+        return Base64.decodeBase64(string);
+    }
+
+    public String encodeByteToBase64String(byte[] b) {
+        return Base64.encodeBase64String(b);
     }
 }
